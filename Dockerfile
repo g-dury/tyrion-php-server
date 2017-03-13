f87dcd7ebe1a4af77bbb496efe7a82490857daf3
Guillaume dury <guillaume.dury@lamplight.me>
2017-03-13 15:42:18 +0800

    Modified the server file and Dockerfile for php version with apache

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
-FROM php:7.0-cli
+FROM php:7.0-apache
 RUN apt-get update
 RUN apt-get install libssl-dev -y
 RUN pecl install mongodb && docker-php-ext-enable mongodb
 RUN echo "extension=mongo.so" > /usr/local/etc/php/conf.d/ext-mongo.ini
-COPY . /usr/src/tyrion-project
-WORKDIR /usr/src/tyrion-project
-CMD [ "php", "./index.php" ]
+COPY . /var/www/html
--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 
-phpinfo();
 
-$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$manager = new MongoDB\Driver\Manager("mongodb://mongodb-mongodb-replicas:27017");
 var_dump($manager);
+
+$filter= [ 'custom_message' =>  ['$exists' => true ] ];
+
+$query = new MongoDB\Driver\Query($filter);
+$rows = $manager->executeQuery('test.test', $query); // $mongo contains the connection object to MongoDB
+foreach($rows as $r){
+   var_dump($r);
+}
+
 
 echo "Hello world";
 
